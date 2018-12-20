b497b9bfeaa4fd4151280b82a60d3d052c932a4f
Bekir Dağ <bekir@piyote.com>
2018-12-20 04:37:11 +0300

    Hex code updated

--- arduino/code/code.ino
+++ arduino/code/code.ino
@@ -17,101 +17,7 @@ int servoDelay = 100;
 
 String inString = "";
 
-int photocellPin = 0;
-int photocellValue = 0;
 
-int micPin = A1;
-int micVal = 0;
-
-float tempC;
-int reading;
-int tempPin = 3;
-
-const int trigPin = 6;
-const int echoPin = 5;
-long duration;
-int distance;
-
-
-int fourty_five = 112;
-int ninety = fourty_five*2;
-                             // 0,  1,  2,  3,  4,  5,  6,  7,  8
-int servoCurrentPositions[] = {315,495,335,535,400,430,410,460,535};
-
-int startPositionX[] = {
-// 0,  1,  2,  3,  4,  5,  6,  7,  8
-  315,495,335,535,400,430,410,460,535
-}; 
-
-
-int startPositionY[] = {
-// 0,  1,  2,  3,  4,  5,  6,  7,  8
-  startPositionX[0]-ninety,
-  startPositionX[1],
-  startPositionX[2]-ninety,
-  startPositionX[3],
-  startPositionX[4]-ninety,
-  startPositionX[5],
-  startPositionX[6]-ninety,
-  startPositionX[7],
-  startPositionX[8]
-}; 
-
-int servoDir[4] = {1,1,-1,-1};
-int move_dir = 0;
-
-void move_body(int servoData[]) {
-
-  if(mode==1 && distance<40){
-    return;  
-  }
-  int legOrder[4] = {1,5,8,7};
-
-  newServoValue(legOrder[0],-100);
-  newServoValue(legOrder[1],-100);
-  delay(servoDelay);
-
-  // up
-  newServoValue(0,fourty_five*servoData[0]);
-  newServoValue(2,fourty_five*servoData[1]);
-  newServoValue(4,fourty_five*servoData[2]);
-  newServoValue(6,fourty_five*servoData[3]);
-  delay(servoDelay);
-
-  newServoValue(legOrder[0],0);
-  newServoValue(legOrder[1],0);
-  delay(servoDelay);
-
-  newServoValue(legOrder[2],-100);
-  newServoValue(legOrder[3],-100);
-  delay(servoDelay);
-
-  // down
-  newServoValue(0,fourty_five*servoData[0]*(-1));
-  newServoValue(2,fourty_five*servoData[1]*(-1));
-  newServoValue(4,fourty_five*servoData[2]*(-1));
-  newServoValue(6,fourty_five*servoData[3]*(-1));
-  delay(servoDelay);
-
-  newServoValue(legOrder[2],0);
-  newServoValue(legOrder[3],0);
-  delay(servoDelay);
-  
-}
-
-void goLay() {
-  newServoValue(1,ninety*(-1));
-  newServoValue(8,ninety*(-1));
-  newServoValue(5,ninety*(-1));
-  newServoValue(7,ninety*(-1));
-}
-
-void goUp() {
-  newServoValue(1,0);
-  newServoValue(8,0);
-  newServoValue(5,0);
-  newServoValue(7,0);
-}
 
 void setup() { 
 
@@ -119,140 +25,52 @@ void setup() {
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
   
   analogReference(INTERNAL);
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600);
   Wire.begin(SLAVE_ADDRESS);
   Wire.onReceive(receiveData);
-
   delay(10);
   
 } 
 
-void standStill(int axis) {
-  if(axis>0){
-    changePos(startPositionX);  
-  }
-  else
-  {
-    changePos(startPositionY);  
-  }
-  
-}
-
-void changePos(int positions[]) {
-  for (int servoNum = 0; servoNum < 8; servoNum++) {
-    int servoControl = servoNum;
-    servoControl = (servoNum==3) ? 8 : servoNum;
-    delay(servoDelay);
-    pwm.setPWM(servoControl, 0, positions[servoNum]);
-    newServoValue(servoControl,0);
-  }
-}
-
-void setDistance(){
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance= duration*0.034/2;
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  //delay(1000);
-}
 
 void loop() { 
-  setDistance();
-  sendData();
-/*
-  photocellValue = analogRead(photocellPin);
-  Serial.println("Light value: ");
-  Serial.println(photocellValue);
-
-  reading = analogRead(tempPin);
-  float voltage = reading * 5.19;
-  voltage /= 1024.0;
-  
-  float temperatureC = (voltage - 0.5) * 100 ;
-  Serial.print(temperatureC); Serial.println(" degrees C");
-  delay(1000);
-
-  micVal = analogRead(micPin);
-  Serial.println("Volume: ");
-  Serial.println (micVal);
-
- RawValue = analogRead(analogIn);
- Voltage = (RawValue / 1024.0) * 5000; // Gets you mV
- Amps = ((Voltage - ACSoffset) / mVperAmp);
-*/
-  
   switch (mode) {
     case 0:
-    move_dir = 0;
-      standStill(1);
-    return;
+      pwm.setPWM(0, 0, 0);
+      pwm.setPWM(1, 0, 0);
+      pwm.setPWM(2, 0, 0);
+      pwm.setPWM(3, 0, 0);
+
+      pwm.setPWM(4, 0, 0);
+      pwm.setPWM(5, 0, 0);
+      pwm.setPWM(6, 0, 0);
+      pwm.setPWM(7, 0, 0);
       break;
     case 1:
-      servoDir[0] = 1;
-      servoDir[1] = 1;
-      servoDir[2] = -1;
-      servoDir[3] = -1;
-      move_dir = 1;
+      pwm.setPWM(0, 0, 180);
+      pwm.setPWM(1, 0, 180);
+      pwm.setPWM(2, 0, 180);
+      pwm.setPWM(3, 0, 180);
+
+      pwm.setPWM(4, 0, 180);
+      pwm.setPWM(5, 0, 180);
+      pwm.setPWM(6, 0, 180);
+      pwm.setPWM(7, 0, 180);
+      
       break;
     case 2:
-      servoDir[0] = 1;
-      servoDir[1] = -1;
-      servoDir[2] = 1;
-      servoDir[3] = -1;
-      move_dir = 1;
-      break;
-    case 3:
-      servoDir[0] = 1;
-      servoDir[1] = -1;
-      servoDir[2] = -1;
-      servoDir[3] = 1;
-      move_dir = 1;
-      break;
-    case 4:
-      servoDir[0] = -1;
-      servoDir[1] = -1;
-      servoDir[2] = 1;
-      servoDir[3] = -1;
-      move_dir = 1;
-      break;
-    case 5:
-      servoDir[0] = -1;
-      servoDir[1] = 1;
-      servoDir[2] = -1;
-      servoDir[3] = 1;
-      move_dir = 1;
-      break;
-    case 6:
-      servoDir[0] = -1;
-      servoDir[1] = 1;
-      servoDir[2] = 1;
-      servoDir[3] = -1;
-      move_dir = 1;
-      break;
-    case 7:
-      standStill(0);
-      move_dir = 0;
-      break;
-    case 8:
-      goUp();
-      move_dir = 0;
-      break;
-    case 9:
-      goLay();
-    move_dir = 0;
+      pwm.setPWM(0, 0, 90);
+      pwm.setPWM(1, 0, 90);
+      pwm.setPWM(2, 0, 90);
+      pwm.setPWM(3, 0, 90);
+
+      pwm.setPWM(4, 0, 90);
+      pwm.setPWM(5, 0, 90);
+      pwm.setPWM(6, 0, 90);
+      pwm.setPWM(7, 0, 90);
+      
       break;
   }
-  if(move_dir > 0) {
-    move_body(servoDir);
-  }
-  
 } 
 
 // callback for received data
@@ -312,22 +130,7 @@ void receiveData(int byteCount) {
   }
 }  // end while
 
-// callback for sending data
-void sendData() {
-  Wire.write(distance);
-  //delay(1000);
-}
 
-void newServoValue(int servoNum,int newVal) {
-    if((startPositionX[servoNum]+newVal<SERVOMAX) && startPositionX[servoNum]-newVal>SERVOMIN) {
-      servoCurrentPositions[servoNum] = startPositionX[servoNum]+newVal;
-      pwm.setPWM(servoNum, 0, servoCurrentPositions[servoNum]);
-    }
-    else{
-      Serial.print("bigger or smaller!");
-      return;  
-    }
-}
 
 
 
