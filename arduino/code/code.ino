cbdd9b0448d0b0256eb0319d6690e4b276f3e20a
Bekir Dağ <bekir@piyote.com>
2019-03-14 23:33:25 +0300

    Hex code updated

--- arduino/code/code.ino
+++ arduino/code/code.ino
@@ -1,15 +1,5 @@
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include <Adafruit_PWMServoDriver.h>
-#include <string.h>
-#include <ctype.h>
-
-
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
-
-
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
@@ -17,16 +7,31 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
 #define SLAVE_ADDRESS 0x04 // raspberry pi connection
 
+char coming_number[50];
 int state = 0;
+
 int raspi_val;
 int mode = 0;
-int servoDelay = 150;
+
+int servoDelay = 120;
+
 String inString = "";
 
-const int trigPin = 8;
-const int echoPin = 9;
+int photocellPin = 0;
+int photocellValue = 0;
+
+int micPin = A1;
+int micVal = 0;
+
+float tempC;
+int reading;
+int tempPin = 3;
+
+const int trigPin = 9;
+const int echoPin = 8;
 long duration;
 int distance;
+
 
 int fourty_five = 112;
 int ninety = fourty_five*2;
@@ -63,6 +68,10 @@ int move_dir = 0;
 
 void move_body_backwards() {
 
+  if(distance<=10) {
+    return;
+  }
+
 
   pwm.setPWM(4, 0, SERVOMAX);
   pwm.setPWM(6, 0, SERVOMAX);
@@ -97,6 +106,10 @@ void move_body_backwards() {
 }
 
 void move_body_forwards() {
+
+  if(distance>=10) {
+    return;
+  }
   
   pwm.setPWM(4, 0, SERVOMAX);
   pwm.setPWM(6, 0, SERVOMAX);
@@ -256,27 +269,34 @@ void turn_right(int degree=ninety) {
   pwm.setPWM(7, 0, SERVOMAX);
 }
 
+void goLay() {
+  newServoValue(1,ninety*(-1));
+  newServoValue(8,ninety*(-1));
+  newServoValue(5,ninety*(-1));
+  newServoValue(7,ninety*(-1));
+}
+
+void goUp() {
+  newServoValue(1,0);
+  newServoValue(8,0);
+  newServoValue(5,0);
+  newServoValue(7,0);
+}
+
 void setup() { 
+
   pwm.begin();
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
   
+  analogReference(INTERNAL);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600);
   Wire.begin(SLAVE_ADDRESS);
   Wire.onReceive(receiveData);
 
   delay(10);
-
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
-  // display oled
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay(); // clearing the display
-  display.setTextColor(WHITE); //setting the color
-  display.setTextSize(1); //set the font size
-  display.setCursor(5,0); //set the cursor coordinates
-  display.print("initializing");
-  display.display();
+  
 } 
 
 void standStill(int axis) {
@@ -293,7 +313,8 @@ void standStill(int axis) {
 void changePos(int positions[]) {
   for (int servoNum = 0; servoNum < 8; servoNum++) {
     int servoControl = servoNum;
-    
+    //servoControl = (servoNum==3) ? 8 : servoNum;
+    delay(servoDelay);
     pwm.setPWM(servoControl, 0, positions[servoNum]);
     //newServoValue(servoControl,0);
   }
@@ -306,23 +327,36 @@ void setDistance(){
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
-
-      // print time
-  display.clearDisplay();
-  display.setTextColor(WHITE); //setting the color
-  display.setTextSize(1); //set the font size
-  display.setCursor(5,0); //set the cursor coordinates
-
-  display.print("Distance: ");
-  display.print(distance);
-  display.print("cm");
-  display.display();
+  distance= duration*0.034/2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  //delay(1000);
 }
 
 void loop() { 
   setDistance();
+  //sendData();
+/*
+  photocellValue = analogRead(photocellPin);
+  Serial.println("Light value: ");
+  Serial.println(photocellValue);
 
+  reading = analogRead(tempPin);
+  float voltage = reading * 5.19;
+  voltage /= 1024.0;
+  
+  float temperatureC = (voltage - 0.5) * 100 ;
+  Serial.print(temperatureC); Serial.println(" degrees C");
+  delay(1000);
+
+  micVal = analogRead(micPin);
+  Serial.println("Volume: ");
+  Serial.println (micVal);
+
+ RawValue = analogRead(analogIn);
+ Voltage = (RawValue / 1024.0) * 5000; // Gets you mV
+ Amps = ((Voltage - ACSoffset) / mVperAmp);
+*/
   
   switch (mode) {
     case 0:
@@ -353,11 +387,11 @@ void loop() {
       move_dir = 0;
       break;
     case 8:
-
+      goUp();
       move_dir = 0;
       break;
     case 9:
-
+      goLay();
       move_dir = 0;
       break;
     case 10:
@@ -369,16 +403,16 @@ void loop() {
       move_dir = 0;
       break;
     case 12:
-      look_at(200);
+      look_at(450);
       break;
     case 13:
-      look_at(300);
+      look_at(550);
       break;
     case 14:
-      look_at(400);
+      look_at(375);
       break;
     case 15:
-      look_at(500);
+      look_at(400);
       break;
       
   }
@@ -466,7 +500,7 @@ void receiveData(int byteCount) {
 
 // callback for sending data
 void sendData() {
-  //Wire.write(distance);
+  Wire.write(distance);
   //delay(1000);
 }
 
